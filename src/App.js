0a5093e9564eecf5d096f4c32cf54c71c6d501bb
darshraundal <darshanraundal55@gmail.com>
2022-04-08 19:11:28 +0530

    2md final comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Component }  from 'react';
+import { Routes, Route } from "react-router-dom";
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import NoPage from './Pages/NoPage';
+import About from './Pages/About';
+import Gallery from './Pages/Gallery';
+import Main from './Iternary/Main';
+import Booknow from './Iternary/Booknow';
+import SubSector from './SubComponent/SubSector';
+import Sector from './Components/Sector';
+import PDFview from './Iternary/PDFview';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Profile from './Components/Profile';
+import Admin from './Components/Admin';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>     
+     <Header/>
+      <Routes>
+         <Route exact path="/" element={<Home />} />
+          <Route path="/:Id" element={<Sector />} />
+          <Route exact path="/profile" element={<Profile/>} />
+          <Route exact path="/Admin" element={<Admin/>} />
+          <Route exact path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/:Id/:Sid" element={<SubSector />} />
+          <Route path="/Details/:SSid" element={<Main />} />
+          <Route path="/book/:bkid/:smid/:cid" element={<Booknow />} />
+          <Route path="/view/:bkid/:smid/:cid/" element={<PDFview />} />
+          <Route path="About" element={<About />} />
+          <Route path="Gallery" element={<Gallery />} />
+          <Route path="*" element={<NoPage />} />
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
